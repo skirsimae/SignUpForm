c6c8a1240119e333218fe8bc9efbdd534a70ea0b
Silva Kirsimae <silvakirsimae@gmail.com>
2023-01-27 13:41:47 +0100

    Add validation to the username

--- SignUpForm/SignUpForm.swift
+++ SignUpForm/SignUpForm.swift
@@ -10,66 +10,74 @@ import Combine
 
 // MARK: - View Model
 class SignUpFormViewModel: ObservableObject {
-  
-  // MARK: Input
-  @Published var username: String = ""
-  @Published var password: String = ""
-  @Published var passwordConfirmation: String = ""
-  
-  // MARK: Output
-  @Published var usernameMessage: String = ""
-  @Published var passwordMessage: String = ""
-  @Published var isValid: Bool = false
+    
+    // MARK: Input
+    @Published var username: String = ""
+    @Published var password: String = ""
+    @Published var passwordConfirmation: String = ""
+    
+    // MARK: Output
+    @Published var usernameMessage: String = ""
+    @Published var passwordMessage: String = ""
+    @Published var isValid: Bool = false
+    
+    private lazy var isUsernameLengthValidPublisher: AnyPublisher<Bool, Never> = {
+      $username
+        .map { $0.count >= 3 }
+        .eraseToAnyPublisher()
+    }()
     
     init() {
-        $username
-            .map { $0.count >= 3 }
+        isUsernameLengthValidPublisher
             .assign(to: &$isValid)
+        isUsernameLengthValidPublisher
+            .map { $0 ? "" : "Username too short. Needs to be at least 3 characters." }
+        .assign(to: &$usernameMessage)
     }
 }
 
 // MARK: - View
 struct SignUpForm: View {
-  @StateObject var viewModel = SignUpFormViewModel()
-  
-  var body: some View {
-    Form {
-      // Username
-      Section {
-        TextField("Username", text: $viewModel.username)
-          .autocapitalization(.none)
-          .disableAutocorrection(true)
-      } footer: {
-        Text(viewModel.usernameMessage)
-          .foregroundColor(.red)
-      }
-      
-      // Password
-      Section {
-        SecureField("Password", text: $viewModel.password)
-        SecureField("Repeat password", text: $viewModel.passwordConfirmation)
-      } footer: {
-        Text(viewModel.passwordMessage)
-          .foregroundColor(.red)
-      }
-      
-      // Submit button
-      Section {
-        Button("Sign up") {
-          print("Signing up as \(viewModel.username)")
+    @StateObject var viewModel = SignUpFormViewModel()
+    
+    var body: some View {
+        Form {
+            // Username
+            Section {
+                TextField("Username", text: $viewModel.username)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+            } footer: {
+                Text(viewModel.usernameMessage)
+                    .foregroundColor(.red)
+            }
+            
+            // Password
+            Section {
+                SecureField("Password", text: $viewModel.password)
+                SecureField("Repeat password", text: $viewModel.passwordConfirmation)
+            } footer: {
+                Text(viewModel.passwordMessage)
+                    .foregroundColor(.red)
+            }
+            
+            // Submit button
+            Section {
+                Button("Sign up") {
+                    print("Signing up as \(viewModel.username)")
+                }
+                .disabled(!viewModel.isValid)
+            }
         }
-        .disabled(!viewModel.isValid)
-      }
     }
-  }
 }
 
 // MARK: - Preview
 struct SignUpForm_Previews: PreviewProvider {
-  static var previews: some View {
-    NavigationStack {
-      SignUpForm()
-        .navigationTitle("Sign up")
+    static var previews: some View {
+        NavigationStack {
+            SignUpForm()
+                .navigationTitle("Sign up")
+        }
     }
-  }
 }
