675c9032aece6e126ba40dc9d7960a8f055b8bd7
Silva Kirsimae <silvakirsimae@gmail.com>
2023-01-30 13:34:00 +0100

    Add enum for username validation

---
+++ SignUpForm/Models/UsernameValid.swift
@@ -0,0 +1,14 @@
+//
+//  UsernameValid.swift
+//  SignUpForm
+//
+//  Created by Silva Kirsimae on 30/01/2023.
+//
+
+import Foundation
+
+enum UsernameValid {
+    case valid
+    case tooShort
+    case notAvailable
+}
